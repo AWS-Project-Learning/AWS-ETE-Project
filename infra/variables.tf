ce893cb42731b7a8daef5525e5cb2f630f58ec1d
Rajeshkoki <rajeshkoki1323@gmail.com>
2026-05-12 12:09:24 +1000

    default region set to us-east-1

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,7 +5,7 @@
 variable "aws_region" {
   description = "AWS region for all resources"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "environment" {
